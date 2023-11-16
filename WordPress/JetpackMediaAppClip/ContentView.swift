870f9e96dc000229456f169955a4eb9ed41e9dad
Povilas Staskus <povilas.staskus@automattic.com>
2023-11-16 12:06:31 +0100

    Created JetpackMediaAppClip target

---
+++ WordPress/JetpackMediaAppClip/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  JetpackMediaAppClip
+//
+//  Created by Povilas Staskus on 16/11/2023.
+//  Copyright © 2023 WordPress. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordPress/JetpackMediaAppClip/JetpackMediaAppClipApp.swift
@@ -0,0 +1,18 @@
+//
+//  JetpackMediaAppClipApp.swift
+//  JetpackMediaAppClip
+//
+//  Created by Povilas Staskus on 16/11/2023.
+//  Copyright © 2023 WordPress. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct JetpackMediaAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
